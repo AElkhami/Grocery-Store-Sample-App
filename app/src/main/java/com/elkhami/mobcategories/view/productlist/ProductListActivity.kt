e3e04d1a5faf0822cad446868992cc51f5d601f9
Ahmed22 <Elkhami.it@gmail.com>
2021-02-20 03:01:57 +0200

    Creating UI layouts. Creating MVP packages.

---
+++ app/src/main/java/com/elkhami/mobcategories/view/productlist/ProductListActivity.kt
@@ -0,0 +1,12 @@
+package com.elkhami.mobcategories.view.productlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elkhami.mobcategories.R
+
+class ProductListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_list)
+    }
+}
