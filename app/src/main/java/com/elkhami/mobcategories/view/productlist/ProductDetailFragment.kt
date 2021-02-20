3431c2afdc78f75886e5c888ff5419cc88011c4c
Ahmed22 <Elkhami.it@gmail.com>
2021-02-20 03:20:28 +0200

    Creating product detail UI layout.

---
+++ app/src/main/java/com/elkhami/mobcategories/view/productlist/ProductDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.elkhami.mobcategories.view.productlist
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.elkhami.mobcategories.R
+
+/**
+ * Created by A.Elkhami on 20,February,2021
+ */
+class ProductDetailFragment: Fragment(R.layout.fragment_product_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
